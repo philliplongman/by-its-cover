d6c98a9735ef8ae19859f7071c7da57f46ce4f63
Phillip Longman <himself@philliplongman.com>
2017-03-08 19:17:14 -0500

    Add readme and badges.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,13 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require "simplecov"
+SimpleCov.start do
+  add_filter "/vendor/" # Ignores any file containing "/vendor/" in its path.
+  # add_filter "/lib/myfile.rb" # Ignores a specific file.
+end
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
