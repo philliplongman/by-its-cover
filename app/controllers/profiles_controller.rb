7d012d39becee5bb892c95e7138035075934f7c6
Phillip Longman <himself@philliplongman.com>
2017-03-17 23:09:10 -0400

    Refactor profiles controller.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,25 +1,22 @@
 class ProfilesController < ApplicationController
 
   def new
-    @user = User.find(params[:user_id])
-    @profile = Profile.new
+    @profile = Profile.new(user_id: params[:user_id])
   end
 
   def create
-    @user = User.find(params[:user_id])
-    @profile = Profile.create profile_params.merge(user: @user)
-    respond_with @profile, location: -> { user_path(@user) }
+    @profile = Profile.create profile_params.merge(user_id: params[:user_id])
+    respond_with @profile, location: -> { user_path(@profile.user) }
   end
 
   def edit
-    @user = User.find(params[:user_id])
-    @profile = @user.profile
+    @profile = Profile.find_by_user_id(params[:user_id])
   end
 
   def update
-    @user = User.find(params[:user_id])
-    @profile = @user.profile.update(profile_params)
-    respond_with @profile, location: -> { user_path(@user) }
+    @profile = Profile.find_by_user_id(params[:user_id])
+    @profile.update(profile_params)
+    respond_with @profile, location: -> { user_path(@profile.user) }
   end
 
   private
