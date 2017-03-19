81aad926c3cc1d3c44c9d72b09c472a97814bf2d
Phillip Longman <himself@philliplongman.com>
2017-03-19 03:36:23 -0400

    Split meta program methods into chunks.

--- app/controllers/concerns/resourceable.rb
+++ app/controllers/concerns/resourceable.rb
@@ -2,13 +2,48 @@ module Resourceable
 
   private
 
+  def access_resource(resource)
+    resource = resource.to_s.downcase.singularize
+
+    create_methods_for_singular_resources resouce
+    create_shared_methods resource
+  end
+
   def access_resources(resource)
     resource = resource.to_s.downcase.singularize
 
+    create_methods_for_plural_resources resouce
+    create_shared_methods resource
+  end
+
+  def create_methods_for_singular_resources(resource)
+  end
+
+  def create_methods_for_plural_resources(resource)
+    # find existing resource by id or return nil
+    define_private_method "existing_#{resource}" do
+      id = send "#{resource}_id_from_params"
+      id.present? ? resource.capitalize.constantize.find(id) : nil
+    end
+
+    # new resource
+    define_private_method "new_#{resource}" do
+      resource.capitalize.constantize.new
+    end
+
+    # find id for resource from params
+    define_private_method "#{resource}_id_from_params" do
+      params["#{resource}_id"] || (params[:id] if controller_name == resource) || nil
+    end
+  end
+
+  def create_shared_methods(resource)
     # getter - collection
     define_private_method resource.pluralize  do
       instance_variable_get("@#{resource.pluralize}") ||
-        instance_variable_set("@#{resource.pluralize}", resource.capitalize.constantize.all)
+        instance_variable_set(
+          "@#{resource.pluralize}", resource.capitalize.constantize.all
+        )
     end
 
     # getter - single record
@@ -24,22 +59,6 @@ module Resourceable
         record.assign_attributes send("#{resource}_params")
       end
       record
-    end
-
-    # find existing resource or return nil
-    define_private_method "existing_#{resource}" do
-      id = send "#{resource}_id_from_params"
-      id.present? ? resource.capitalize.constantize.find(id) : nil
-    end
-
-    # new resource
-    define_private_method "new_#{resource}" do
-      resource.capitalize.constantize.new
-    end
-
-    # find id for resource from params
-    define_private_method "#{resource}_id_from_params" do
-      params["#{resource}_id"] || (params[:id] if controller_name == resource) || nil
     end
 
     # return strong params for resource
