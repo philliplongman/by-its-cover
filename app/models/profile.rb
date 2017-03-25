c52ef0eb73da38fe066669f6d12b6b90540871a0
Phillip Longman <himself@philliplongman.com>
2017-03-25 02:29:10 -0400

    Freeze constant literals.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -24,7 +24,7 @@ class Profile < ApplicationRecord
 
   GENDERS = [
     "Male", "Female", "Trans Male", "Trans Female", "Other/Non-Conforming"
-  ]
+  ].freeze
 
   validates :username, presence: true
   validates :username, uniqueness: { case_sensitive: false }
