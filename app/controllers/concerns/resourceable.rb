ff5f0834dea8427ccb9bd3a94c139f34cf23f3aa
Phillip Longman <himself@philliplongman.com>
2017-04-19 13:54:46 -0400

    Update Reasourceable module.

--- app/controllers/concerns/resourceable.rb
+++ app/controllers/concerns/resourceable.rb
@@ -5,7 +5,7 @@ require "resourceable/accessor"
 # Module to DRY controllers by encapsulating finding/initializing ActiveRecord
 # resources and assigning params data in a PORO. ::access_resource is called at
 # the top of the controller for every resource you want to access, and two
-# memo-ized getter methods are metaprogramatically defined for each. When
+# memoized getter methods are metaprogramatically defined for each. When
 # called, they generate an Accessor object that returns the correct object,
 # loaded with the params, but not saved.
 module Resourceable
@@ -16,18 +16,23 @@ module Resourceable
     # Accepts a list of ActiveRecord models as symbols, strings, or constants.
     # Can also accept a single model and the following keyword options:
     #
-    # key:
-    #   The db column to use for finding the resource in this context.
     # columns:
     #   The columns you want to permit access to in this context.
     #   When the resource is loaded, data for these columns will be
-    #   automatically assigned using strong params
+    #   automatically assigned using strong params.
+    # decorator:
+    #   For use with the Draper gem. When true, return a the class wrapped in
+    #   a decorator. A decorator class can be specified, instead, if named
+    #   differently than the resource.
+    # key:
+    #   The db column to use for finding the resource in this context.
     #
     # Examples:
     #   access_resources :users, :accounts
-    #   access_resource Profile, key: :user_id, columns: [:username, :picture]
+    #   access_resource Profile, key: :user_id, decorator: true,
+    #     columns: [:username, :picture]
     #
-    define_access_methods_for models.pop, options if options.present?
+    define_access_methods_for(models.pop, options) if options.present?
 
     models.each { |model| define_access_methods_for model }
   end
@@ -42,19 +47,37 @@ module Resourceable
   end
 
   def define_memoized_getter_for_collection(resource, options = {})
-    # resource & options will be hard-coded into the method
-    # params will get called when the methods is called
-    define_method resource.pluralize.to_sym do
+    # Generate a memoized getter method to return a collection of ActiveRecord
+    # objects. The resource & options args will be hard-coded into the method.
+    #
+    # The generated method will accept a block for chaining ActiveRecord
+    # methods. params & block will be called when the method is called.
+    #
+    # example usage:
+    #   def index
+    #     users { |u| u.limit(5) }
+    #   end
+    #
+    define_method resource.pluralize.to_sym do |&scope|
       instance_variable_get("@#{resource.pluralize}") || begin
-        accessor = Accessor.new(resource, options, params)
+        accessor = Accessor.new(resource, options, params, &scope)
         instance_variable_set "@#{resource.pluralize}", accessor.collection
       end
     end
   end
 
   def define_memoized_getter_for_single_record(resource, options = {})
-    # resource & options will be hard-coded into the method
-    # params will get called when the methods is called
+    # Generate a memoized getter method to return a single ActiveRecord
+    # object, loaded with data from params. The resource & options args
+    # will be hard-coded into the method.
+    #
+    # params will be called when the method is called.
+    #
+    # example usage:
+    #   def show
+    #     user
+    #   end
+    #
     define_method resource.to_sym do
       instance_variable_get("@#{resource}") || begin
         accessor = Accessor.new(resource, options, params)
--- app/controllers/concerns/resourceable/accessor.rb
+++ app/controllers/concerns/resourceable/accessor.rb
@@ -6,30 +6,42 @@ module Resourceable
   class Accessor
     attr_reader :resource, :permitted_columns
 
-    def initialize(resource, options, params)
+    def initialize(resource, options, params, &scope)
       @resource = resource.to_s.underscore.singularize
       @klass = resource.camelize.constantize
+
+      @decorator = options[:decorator]
       @key = options[:key]
       @permitted_columns = options[:columns]
+
       @params = params
+      @scope = scope
     end
 
     def collection
-      klass.all
+      scope ? scope.yield(klass) : klass.all
     end
 
     def load_resource
-      record = klass.find_or_initialize_by(identifier)
-      if params[resource].present?
-        record.assign_attributes association_params
-        record.assign_attributes resource_params
-      end
-      record
+      loaded = existing_resource || new_resource
+      decorate(loaded).tap{ |r| r.assign_attributes updated_attributes }
     end
 
     private
 
-    attr_reader :key, :klass, :params
+    attr_reader :decorator, :key, :klass, :params, :scope
+
+    def existing_resource
+      klass.find_by(identifier) unless new_or_create_action?
+    end
+
+    def new_resource
+      klass.new identifier
+    end
+
+    def new_or_create_action?
+      params[:action].in? %w(new create)
+    end
 
     def identifier
       key ? { key => params[key] } : { id: resource_id_from_params }
@@ -43,18 +55,27 @@ module Resourceable
       params[:controller] == resource.pluralize
     end
 
-    def association_params
-      params.require(resource).permit(associations)
-    end
-
-    def associations
-      (klass.column_names - [key.to_s]).select do |column|
-        column.end_with? "_id"
-      end
+    def updated_attributes
+      params[resource].present? ? resource_params : {}
     end
 
     def resource_params
-      params.require(resource).permit(permitted_columns)
+      params.require(resource).permit(associations, permitted_columns)
+    end
+
+    def associations
+      (klass.column_names - [key.to_s]).select { |col| col.end_with? "_id" }
+    end
+
+    def decorate(record)
+      case decorator
+      when nil, false
+        record
+      when true
+        record.decorate
+      else
+        decorator.to_s.camelize.constantize.new record
+      end
     end
 
   end
