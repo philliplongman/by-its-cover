98a9a8407571cb369c89efc306fab534961b1124
Phillip Longman <himself@philliplongman.com>
2017-05-10 23:48:12 -0400

    Make matcher generic.

--- spec/support/matchers/have_record_not_saved_alert.rb
+++ spec/support/matchers/have_record_not_saved_alert.rb
@@ -1,9 +1,10 @@
 module FeaturesMatchers
   extend RSpec::Matchers::DSL
 
-  matcher :have_profile_not_saved_alert do
+  matcher :have_record_not_saved_alert do
 
-    message = "Unable to save profile. See errors below."
+    message = /^Unable to save .+\. See errors below\./
+              # "Unable to save record. See errors below."
 
     match_unless_raises do |page|
       expect(page).to have_css "div.alert-box.alert", text: message
